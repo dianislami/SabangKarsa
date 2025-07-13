3d53d816a2cee9fadc74445ede5283bf8e11e7d3
naufalhanif25 <naufal.hanif2525@gmail.com>
2025-07-13 10:01:08 +0700

    feat: add JakSabang AI Chatbot

---
+++ src/components/ui/chatbot-button.tsx
@@ -0,0 +1,30 @@
+import { MessageCircle } from "lucide-react"
+import { ChatbotOverlay } from "./chatbot-overlay"
+import { useState, useRef, useEffect } from "react"
+
+export function ChatbotButton() {
+    const [visibility, setVisibility] = useState(false);
+    const overlayRef = useRef<HTMLDivElement | null>(null);
+    const visibilityToggle = () => setVisibility(prev => !prev);
+
+    useEffect(() => {
+        function handleClickOutside(event: MouseEvent) {
+            if (overlayRef.current && !overlayRef.current.contains(event.target as Node)) setVisibility(false);
+        }
+
+        if (visibility) document.addEventListener("mousedown", handleClickOutside);
+        else document.removeEventListener("mousedown", handleClickOutside);
+
+        return () => document.removeEventListener("mousedown", handleClickOutside);
+    }, [visibility]);
+
+    return (
+        <>
+            <ChatbotOverlay showOverlay={visibility} ref={overlayRef} />
+            
+            <button onClick={visibilityToggle} className="bg-(--background) z-10 shadow-sm hover:-translate-y-2 transition-transform duration-300 ease-out cursor-pointer fixed right-8 bottom-8 p-[12px] rounded-full">
+                <MessageCircle className="stroke-emerald-600" />
+            </button>
+        </>
+    );
+}
