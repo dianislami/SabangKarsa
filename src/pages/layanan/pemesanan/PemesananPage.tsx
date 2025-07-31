6366dd69416e0f1c699241cc796b633eb246bf06
Muhammad Aidil Fitrah <muh.aidil@mhs.usk.ac.id>
2025-08-01 02:25:06 +0700

    styling: minor update

--- src/pages/layanan/pemesanan/PemesananPage.tsx
+++ src/pages/layanan/pemesanan/PemesananPage.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
-import { ArrowLeft, Car, Home, User, Calendar, MapPin, CreditCard, Package, Mail } from 'lucide-react';
+import { ArrowLeft, Car, Home, User, Calendar, MapPin, CreditCard, Package} from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { ThemeToggle } from '@/components/theme-toogle';
 import { Footer } from '@/components/layouts/footer';
