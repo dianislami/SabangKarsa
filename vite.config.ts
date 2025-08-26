e7fc91dfc2b6422799f0930ec85345f73cac0bef
naufalhanif25 <naufal.hanif2525@gmail.com>
2025-08-26 21:18:27 +0700

    feat: update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,11 @@ import path from "path"
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss()  
   ],
-    resolve: {
+  resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
+  }
 })
