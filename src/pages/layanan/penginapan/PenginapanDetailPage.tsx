ffd64927912e427a73cb94d1765f3f8730cebc7a
naufalhanif25 <naufal.hanif2525@gmail.com>
2025-08-31 13:01:49 +0700

    fix: fix some bugs

--- src/pages/layanan/penginapan/PenginapanDetailPage.tsx
+++ src/pages/layanan/penginapan/PenginapanDetailPage.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, use } from "react";
+import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import { Star, MapPin, Phone, Mail, BedDouble, Landmark } from "lucide-react";
